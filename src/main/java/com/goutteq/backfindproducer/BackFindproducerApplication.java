6e5f1428eec247d94036b47bc6ad84eaee5bcd71
qgoutte <quentingoutte@gmail.com>
2021-05-03 21:51:27 +0200

    initial repository

---
+++ src/main/java/com/goutteq/backfindproducer/BackFindproducerApplication.java
@@ -0,0 +1,13 @@
+package com.goutteq.backfindproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFindproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFindproducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goutteq/backfindproducer/BackFindproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goutteq.backfindproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFindproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
